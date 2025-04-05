2d1a9548c014ab6c37cc9d8c1a6c2ab42c7105fe
PrernaBadwane <95902077+PrernaBadwane@users.noreply.github.com>
2025-04-05 23:45:31 +0530

    container added

---
+++ src/assets/index.ts
@@ -0,0 +1,12 @@
+// Icons
+import horizon from "../assets/Icons/Horizon.png";
+
+// Images
+
+export const VIDEOS = {
+  authVid: "/videos/authVid.mp4", 
+};
+
+export const ICONS = {
+  horizon,
+};
